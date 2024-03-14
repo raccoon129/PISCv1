a8102f0c1fb9c4b74e6e233b28a66ffba302b014
Emanuel Santana <106712653+raccoon129@users.noreply.github.com>
2024-03-14 00:19:39 -0600

    Update db.php
    db actualizado

--- db.php
+++ db.php
@@ -4,10 +4,10 @@
 
 
 // Esta es para conectarla al localhost
-$conexion=mysqli_connect("localhost","root","","dbprestamosisc");
+//$conexion=mysqli_connect("localhost","root","","dbprestamosisc");
 
 //esta es para conectarlo a la base de datos en la nube
-//$conexion=mysqli_connect("db4free.net","usuarioadminisc","sistemas","dbprestamosisc_1");
+$conexion=mysqli_connect("db4free.net","usuarioadminisc","sistemas","dbprestamosisc_1");
 $conexion->set_charset("utf8mb4");
 
 
